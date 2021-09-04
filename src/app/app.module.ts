ca1b9017ace6712a5135a5dfe8a7e4ce402607e6
Ajay Chaurasiya <ajaykumarchaurasiya9@gmail.com>
2021-09-04 19:47:10 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { CustomElementsModule } from './components/elements-module/custom-elements.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
